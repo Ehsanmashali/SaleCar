081c5a314448fb7b381bce62346e49c49d4c0635
192.168.100.63 <>
2023-04-09 14:42:33 +0330

    add icon & template card for cars

---
+++ components/module/Card.js
@@ -0,0 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
+import Location from "../icons/Location";
+import styles from "./Card.module.css";
+
+const Card = (props) => {
+  const { id, name, model, year, distance, location, image, price } = props;
+
+  return (
+    <div className={styles.container}>
+      <img src={image} alt="car" className={styles.image} />
+      <h4 className={styles.title}>{`${name} ${model}`}</h4>
+      <p className={styles.detail}>{`${year} . ${distance}km`}</p>
+      <div className={styles.footer}>
+        <p className={styles.price}>$ {price}</p>
+        <div className={styles.location}>
+          <p>{location}</p>
+          <Location />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
