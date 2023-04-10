ae42698531daf733b92d2cd129d98a8f3699e1e0
192.168.100.63 <>
2023-04-10 10:04:27 +0330

    add categories page & fix responsive card

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,9 +1,11 @@
 import CarsPage from "component/components/templates/CarsPage";
 import carsData from "../../data/carsData";
+import Categories from "component/components/module/Categoreis";
 
 const Details = () => {
   return (
     <div>
+      <Categories />
       <CarsPage data={carsData} />
     </div>
   );
