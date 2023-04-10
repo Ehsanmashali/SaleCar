375656b6f5135b80c1d755066543b017f5c10098
192.168.100.63 <>
2023-04-10 16:58:13 +0330

    add search price car

--- pages/cars/index.js
+++ pages/cars/index.js
@@ -1,10 +1,12 @@
 import CarsPage from "component/components/templates/CarsPage";
 import carsData from "../../data/carsData";
 import Categories from "component/components/module/Categoreis";
+import SearchBar from "component/components/module/SearchBar";
 
 const Details = () => {
   return (
     <div>
+      <SearchBar />
       <Categories />
       <CarsPage data={carsData} />
     </div>
